7724d4f826dacbda3f9ef5fca008ffde60c64c20
JJingu <75621973+JJingu@users.noreply.github.com>
2021-12-19 22:06:27 +0900

    django course 17 commit

--- accountapp/views.py
+++ accountapp/views.py
@@ -2,20 +2,23 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 
 # Create your views here.
-
+from accountapp.models import HelloWorld
 
 
 def hello_world(request):
     if request.method == "POST":
-        return render(request, 'accountapp/hello_world.html', context={'text': 'POST METHOD'})
+
+        temp = request.POST.get('hello_world_input')
+
+        new_hello_world = HelloWorld()
+        new_hello_world.text = temp
+        new_hello_world.save()
+
+        return render(request, 'accountapp/hello_world.html', context={'hello_world_output': new_hello_world})
     else:
         return render(request, 'accountapp/hello_world.html', context={'text': 'GET METHOD'})
 
-    #     temp = request.POST.get('hello_world_input')
-    #
-    #     new_hello_world = HelloWorld()
-    #     new_hello_world.text = temp
-    #     new_hello_world.save()
+
     #
     #     return HttpResponseRedirect(reverse('accountapp:hello_world'))
     #

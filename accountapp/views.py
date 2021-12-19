dba58875cb4f62b39d3f43e1fd38aad72146444e
JJingu <75621973+JJingu@users.noreply.github.com>
2021-12-19 21:35:35 +0900

    django course 16 commit

--- accountapp/views.py
+++ accountapp/views.py
@@ -6,5 +6,21 @@ from django.http import HttpResponse, HttpResponseRedirect
 
 
 def hello_world(request):
-    # return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
-    return render(request, 'accountapp/hello_world.html')
+    if request.method == "POST":
+        return render(request, 'accountapp/hello_world.html', context={'text': 'POST METHOD'})
+    else:
+        return render(request, 'accountapp/hello_world.html', context={'text': 'GET METHOD'})
+
+    #     temp = request.POST.get('hello_world_input')
+    #
+    #     new_hello_world = HelloWorld()
+    #     new_hello_world.text = temp
+    #     new_hello_world.save()
+    #
+    #     return HttpResponseRedirect(reverse('accountapp:hello_world'))
+    #
+    # else:
+    #     hello_world_list = HelloWorld.objects.all()
+    #     return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
+
+    # return render(request, 'accountapp/hello_world.html')
